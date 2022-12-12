3ea25db96e3fa4a6a666052d6fe5d0661262df0a
selsh <selmene.far@gmail.com>
2022-12-12 17:16:37 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/emptyactivity/LoginActivity.java
+++ app/src/main/java/com/example/emptyactivity/LoginActivity.java
@@ -2,13 +2,20 @@ package com.example.emptyactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private long backPressedTime;
+    private EditText loginUsername, loginPwd;
+    private Button loginBtn;
+    private TextView loginRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +24,21 @@ public class LoginActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.loginTitle);
         int unicode = 0x1F601;
-
         String emoji = getEmoji(unicode);
-
         textView.setText("Content de vous revoir" + emoji);
+
+        loginUsername = findViewById(R.id.loginUsername);
+        loginPwd = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginButton);
+        loginRegister = findViewById(R.id.loginRegister);
+
+        loginRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
     public String getEmoji(int uni) {
         return new String(Character.toChars(uni));
