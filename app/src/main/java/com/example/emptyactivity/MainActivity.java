c7b979f3304c53208429f777bfb31b987e100dd0
ThomasW2P <t.fourties@icloud.com>
2022-12-12 16:03:41 +0100

    remove loader + back press alert

--- app/src/main/java/com/example/emptyactivity/MainActivity.java
+++ app/src/main/java/com/example/emptyactivity/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.emptyactivity;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import pl.droidsonroids.gif.GifImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private long backPressedTime;
 
     private static final int SPLASH = 3300;
 
@@ -27,12 +35,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
-        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
         imageView = findViewById(R.id.imageView);
-        textView = findViewById(R.id.text);
-
         imageView.setAnimation(topAnim);
-        textView.setAnimation(bottomAnim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -42,5 +46,18 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+    if(backPressedTime + 2000 > System.currentTimeMillis()){
+            super.onBackPressed();
+            return;
+        }else{
+            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+        }
+        backPressedTime = System.currentTimeMillis();
     }
 }
